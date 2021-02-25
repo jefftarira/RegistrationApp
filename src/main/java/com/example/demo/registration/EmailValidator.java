f42172b0fee5449fa1b385fdb2b9f8abd2c18208
Jefferson Tarira <jatt6076@gmail.com>
2021-02-24 23:07:13 -0500

    finish tutorial

--- src/main/java/com/example/demo/registration/EmailValidator.java
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -3,13 +3,18 @@ package com.example.demo.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+  private static final String regex = "^(.+)@(.+)$";
 
   @Override
-  public boolean test(String s) {
+  public boolean test(String email) {
     //    TODO: Regex to validate email
-    return true;
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
   }
 }
